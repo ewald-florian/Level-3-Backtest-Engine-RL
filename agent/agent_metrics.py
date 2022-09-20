fc02dcd2638ddb1b2f366ff4be38a12abbfa35db
Florian Ewald <mirro.gallier@gmail.com>
2022-09-20 20:38:31 +0200

    debugged

--- agent/agent_metrics.py
+++ agent/agent_metrics.py
@@ -9,6 +9,8 @@
 
 # ---------------------------------------------------------------------------
 import textwrap
+import copy
+
 
 from agent.agent_order import OrderManagementSystem as OMS
 from agent.agent_trade import AgentTrade
@@ -37,7 +39,6 @@ class AgentMetrics:
         self.order_list = OMS.order_list
         self.trade_list = AgentTrade.history
 
-
     def get_filtered_messages(self, side=None, template_id=None):
         """
         Filter messages by side (1: Bid, 2: Ask) and template_id:
@@ -59,13 +60,14 @@ class AgentMetrics:
             assert template_id in [99999, 66666, 11111, 33333], \
                 "(AgentMetrics) template_id not valid"
 
-        filtered_messages = OMS.order_list
+        filtered_messages = self.order_list
 
         if filtered_messages:
             # orders must have requested market_id
             if side:
                 filtered_messages = filter(lambda d: d['side'] == side,
                                            filtered_messages)
+
             # orders must have requested side
             if template_id:
                 filtered_messages = filter(lambda d: d['template_id'] == template_id,
@@ -120,12 +122,34 @@ class AgentMetrics:
         assert side in [1, 2], "(AgentMetrics) Side " \
                                "not valid, must be 1 or 2"
 
-        filtered_trades = self.trade_list
+        if self.trade_list:
 
-        filtered_trades = filter(lambda d: d['agent_side'] == side,
-                                 filtered_trades)
+            filtered_trades = self.trade_list
 
-        return list(filtered_trades)
+            filtered_trades = filter(lambda d: d['agent_side'] == side,
+                                     filtered_trades)
+
+            return list(filtered_trades)
+
+    @staticmethod
+    def get_filtered_trades_static(side):
+        """
+        Filter trades by side (1 Buy, 2 Sell)
+
+        :param side
+            int, 1 or 2
+        """
+        assert side in [1, 2], "(AgentMetrics) Side " \
+                               "not valid, must be 1 or 2"
+
+        if self.trade_list:
+
+            filtered_trades = AgentTrade.history
+
+            filtered_trades = filter(lambda d: d['agent_side'] == side,
+                                     filtered_trades)
+
+            return list(filtered_trades)
 
     @property
     def unrealized_quantity(self):
@@ -134,7 +158,7 @@ class AgentMetrics:
         """
         unrealized_quantity = 0
 
-        for trade in AgentTrade.history:
+        for trade in self.trade_list:
 
             quantity = trade['executed_volume'] * 1e-4
             side = trade['agent_side']
@@ -148,16 +172,27 @@ class AgentMetrics:
 
     @property
     def realized_quantity(self):
-        # realized quantity
-        realized_trades = self.get_realized_trades
-        unrealized_trades = self.get_unrealized_trades
 
-        realized_buy_trades = list(filter(lambda d: d['agent_side'] == 1, realized_trades))
-        realized_sell_trades = list(filter(lambda d: d['agent_side'] == 2, realized_trades))
-        realized_buy_quantity = sum(trade['executed_volume'] for trade in realized_buy_trades)
-        realized_sell_quantity = sum(trade['executed_volume'] for trade in realized_sell_trades)
+        realized_quantity = 0
 
-        realized_quantity = realized_buy_quantity - realized_sell_quantity
+        if self.get_realized_trades:
+            # realized quantity
+            realized_trades = self.get_realized_trades
+            unrealized_trades = self.get_unrealized_trades
+
+            realized_buy_trades = list(filter(lambda d: d['agent_side'] == 1,
+                                              realized_trades))
+
+            realized_sell_trades = list(filter(lambda d: d['agent_side'] == 2,
+                                               realized_trades))
+
+            realized_buy_quantity = sum(trade['executed_volume'] for trade in
+                                        realized_buy_trades)
+
+            realized_sell_quantity = sum(trade['executed_volume'] for trade in
+                                         realized_sell_trades)
+
+            realized_quantity = realized_buy_quantity - realized_sell_quantity
 
         return realized_quantity * 1e-4
 
@@ -167,7 +202,9 @@ class AgentMetrics:
         Current value of unrealized position, approximated with best-bid
         or best-ask value.
         """
+
         unrealized_quantity = self.unrealized_quantity
+
         if unrealized_quantity > 0:
             position_value = unrealized_quantity * \
                              Market.instances['ID'].best_bid
@@ -193,7 +230,7 @@ class AgentMetrics:
         #TODO: change: exposure based on buy-in price or current marekt price -> market price
         exposure = 0
 
-        for trade in AgentTrade.history:
+        for trade in self.trade_list:
 
             side = trade['agent_side']
             price = trade['execution_price'] * 1e-8
@@ -240,14 +277,20 @@ class AgentMetrics:
 
         # 'long'
         if self.exposure > 0:
-            buy_trades = list(filter(lambda d: d['agent_side'] == 1, AgentTrade.history))
+            buy_trades = list(filter(lambda d: d['agent_side'] == 1,
+                                     self.trade_list))
+
             # sort by timestamp descending -> latest trade in the beginning of the list
-            sorted_trades = sorted(buy_trades, key=lambda d: d['execution_time'])
+            sorted_trades = sorted(buy_trades, key=lambda d: d[
+                'execution_time'])
 
         # 'short'
         elif self.exposure < 0:
-            sell_trades = list(filter(lambda d: d['agent_side'] == 2, AgentTrade.history))
-            sorted_trades = sorted(sell_trades, key=lambda d: d['execution_time'])
+
+            sell_trades = list(filter(lambda d: d['agent_side'] == 2,
+                                      self.trade_list))
+            sorted_trades = sorted(sell_trades, key=lambda d: d[
+                'execution_time'])
 
         else:
             pass  # 'neutral' / exposure = 0 / no unrealized trades
@@ -275,13 +318,16 @@ class AgentMetrics:
         """
         List of realized trades.
         """
-        # TODO: checken was passiert wenn listen unvollständig sind...(am anfang)
-        realized_trades = AgentTrade.history.copy()
+        # TODO: Refactor...
+        realized_trades = copy.copy(self.trade_list)
         unrealized_trades = self.get_unrealized_trades
 
-        if realized_trades:
+        if realized_trades and unrealized_trades:
+
             for trade in unrealized_trades:
-                realized_trades.remove(trade)
+
+                if trade in realized_trades:
+                    realized_trades.remove(trade)
 
             return realized_trades
 
@@ -291,17 +337,24 @@ class AgentMetrics:
         Vwap buy.
         """
         # TODO: should the prices of unrealized trades be included?
-        realized_trades = self.get_realized_trades
+        vwap_buy= None
 
-        realized_buy_trades = list(filter(lambda d: d['agent_side'] == 1, realized_trades))
-        if realized_buy_trades:
-            vwap_buy = sum(trade['execution_price'] * trade['executed_volume'] for trade in
-                           realized_buy_trades) / sum(trade['executed_volume']
-                                                      for trade in realized_buy_trades) * 1e-8
-            vwap_buy = round(vwap_buy, 2)
-        else:
-            vwap_buy = 0
+        if self.get_realized_trades:
 
+            realized_trades = self.get_realized_trades
+
+            realized_buy_trades = list(filter(lambda d: d['agent_side'] == 1,
+                                              realized_trades))
+
+            if realized_buy_trades:
+
+                vwap_buy = sum(trade['execution_price'] *
+                                trade['executed_volume']
+                                for trade in realized_buy_trades) / sum(
+                                trade['executed_volume']
+                                for trade in realized_buy_trades) * 1e-8
+
+                vwap_buy = round(vwap_buy, 2)
 
         return vwap_buy
 
@@ -310,16 +363,27 @@ class AgentMetrics:
         """
         Vwap sell.
         """
-        realized_trades = self.get_realized_trades
 
-        realized_buy_trades = list(filter(lambda d: d['agent_side'] == 2, realized_trades))
-        if realized_buy_trades:
-            vwap_sell = sum(trade['execution_price'] * trade['executed_volume'] for trade in
-                            realized_buy_trades) / sum(trade['executed_volume']
-                                                       for trade in realized_buy_trades) * 1e-8
-            vwap_sell = round(vwap_sell, 2)
-        else:
-            vwap_sell = None
+        vwap_sell = None
+
+        if self.get_realized_trades:
+
+            realized_trades = self.get_realized_trades
+
+            realized_buy_trades = list(filter(lambda d: d['agent_side'] == 2,
+                                              realized_trades))
+
+            if realized_buy_trades:
+
+                vwap_sell = sum(trade['execution_price'] *
+                                trade['executed_volume']
+                                for trade in realized_buy_trades) / sum(
+                                trade['executed_volume']
+                                for trade in realized_buy_trades) * 1e-8
+                vwap_sell = round(vwap_sell, 2)
+            else:
+                vwap_sell = None
+
         return vwap_sell
 
     @property
@@ -329,6 +393,8 @@ class AgentMetrics:
         :return
         """
         # realized quantity
+
+        vwap_score = None
 
         market_trades = MarketTrade.history
 
@@ -349,15 +415,22 @@ class AgentMetrics:
             if not vwap_sell:
                 vwap_sell = 0
 
-            realized_buy_trades = list(filter(lambda d: d['agent_side'] == 1, realized_trades))
-            realized_sell_trades = list(filter(lambda d: d['agent_side'] == 2, realized_trades))
+            if vwap_buy or vwap_sell:
 
-            qt_bought = sum(trade['executed_volume'] for trade in realized_buy_trades)
-            qt_sold = sum(trade['executed_volume'] for trade in realized_sell_trades)
+                realized_buy_trades = list(filter(lambda d: d['agent_side'] == 1,
+                                                  realized_trades))
 
-            vwap_score = qt_bought * (vwap_symbol - vwap_buy) + qt_sold * (vwap_sell - vwap_symbol)
-        else:
-            vwap_score = 0
+                realized_sell_trades = list(filter(lambda d: d['agent_side'] == 2,
+                                                   realized_trades))
+
+                qt_bought = sum(trade['executed_volume']
+                                for trade in realized_buy_trades)
+                qt_sold = sum(trade['executed_volume']
+                              for trade in realized_sell_trades)
+
+                vwap_score = (qt_bought * (vwap_symbol - vwap_buy) +
+                              qt_sold * (vwap_sell - vwap_symbol))
+
         # TODO: scale?
         return vwap_score
 
@@ -373,14 +446,21 @@ class AgentMetrics:
         if realized_quantity:
             # net long
             if realized_quantity > 0:
+
                 pnl_realized = realized_quantity * (vwap_sell - vwap_buy)
+
             # net short
             elif realized_quantity < 0:
+
                 pnl_realized = realized_quantity * (vwap_buy - vwap_sell)
+
             # no realized quantity
             else:
+
                 pnl_realized = 0
+
         else:
+
             pnl_realized = 0
 
         return round(pnl_realized, 2)
@@ -393,23 +473,31 @@ class AgentMetrics:
         unrealized_trades = self.get_unrealized_trades
         unrealized_quantity = self.unrealized_quantity
 
+        pnl_unreal = 0
+
         if unrealized_trades:
-                # average share price of unrealized trades
-                asp = sum(trade['execution_price'] * trade['executed_volume'] for
-                          trade in unrealized_trades) / sum(trade['executed_volume']
-                                                            for trade in unrealized_trades)
 
-                # net long position -> sell for current best-bid
-                if unrealized_quantity > 0:
-                    immediate_price = Market.instances['ID'].best_bid
+            # average share price of unrealized trades
+            asp = sum(trade['execution_price'] * trade['executed_volume']
+                                 for trade in unrealized_trades) / sum(
+                                 trade['executed_volume']
+                                 for trade in unrealized_trades)
 
-                # net short position -> buy back for current best ask
-                elif unrealized_quantity < 0:
-                    immediate_price = Market.instances['ID'].best_ask
+            # net long position -> sell for current best-bid
+            if unrealized_quantity > 0:
 
-                pnl_unreal = unrealized_quantity * (immediate_price - asp) * 1e-8 * 1e-4
-        else:
-            pnl_unreal = 0
+                immediate_price = Market.instances['ID'].best_bid
+
+            # net short position -> buy back for current best ask
+            elif unrealized_quantity < 0:
+
+                immediate_price = Market.instances['ID'].best_ask
+
+            if unrealized_quantity != 0:
+
+                pnl_unreal = (unrealized_quantity
+                              * (immediate_price - asp)
+                              * 1e-8 * 1e-4)
 
         return round(pnl_unreal, 2)
 
@@ -443,7 +531,7 @@ class AgentMetrics:
         # string representation
         string = f"""
         ----------------------------
-        timestamp:      {timestamp}
+        unixtime:      {timestamp}
         datetime:       {dt_timestamp}
         ---
         exposure:       {self.exposure}
