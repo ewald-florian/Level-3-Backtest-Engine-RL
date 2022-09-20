120d0ce1dddab3de40778dbd533ca60b9f43b538
Florian Ewald <mirro.gallier@gmail.com>
2022-09-20 20:38:47 +0200

    update

--- test_script.py
+++ test_script.py
@@ -26,11 +26,46 @@ if __name__ == '__main__':
     obs_space = ObservationSpace()
     reward = Reward()
     am = AgentMetrics()
+    mi = MarketInterface()
 
     for i in range(replay.episode.__len__()):
 
         replay.step()
-        print(i)
+        # collect context
+        Context(Market.instances['ID'].state_l3)
+        # generate orders
+        if i % 1000 == 0:
+            limit = mf.best_bid()
+            mi.submit_order(side=2, quantity=2220000, limit=limit)
+
+        if i % 1000 == 500:
+            limit = mf.best_ask()
+            mi.submit_order(side=1, quantity=2220000, limit=limit)
+
+        # AgentMetrics FEBUGGING
+        print("filtered messages", am.get_filtered_messages(side=2))
+        print("fm static", am.get_filtered_messages_static(side=2))
+        print("gft", am.get_filtered_trades(side=2))
+        print("get realized trades:", am.get_realized_trades)
+        print("uq", am.unrealized_quantity)
+        print("rq", am.realized_quantity)
+        print("pv", am.position_value)
+        print("exp", am.exposure)
+        print("cp", am.cash_position)
+        print("gzt", am.get_unrealized_trades)
+        print("VWAP buy", am.vwap_buy)
+        print("VWAP sell", am.vwap_sell)
+        print("vscore", am.vwap_score)
+        print("pnl_real", am.pnl_realized)
+        print("pnl_unreal", am.pnl_unrealized)
+        print("exp_left", am.exposure_budget_left)
+        print("tc", am.transaction_costs)
+        print(am) # string repesentation
+        print()
+        print()
+
+
+
 
 
 
