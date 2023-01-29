4e94da2a8f2284f3fd51790a6822f1cc56702f44
florianewald <mirro.gallier@gmail.com>
2023-01-29 01:37:20 +0100

    initial market

--- benchmark_strategies/initial_market_order_benchmark_agent.py
+++ benchmark_strategies/initial_market_order_benchmark_agent.py
@@ -6,6 +6,7 @@ in the first step of the episode. I compute the resulting implementation
 shortfall as benchmark for optimal execution strategies.
 """
 
+
 import numpy as np
 import pandas as pd
 
@@ -16,6 +17,9 @@ from agent.agent_metrics import AgentMetrics
 from agent.agent_order import OrderManagementSystem as OMS
 from agent.agent_trade import AgentTrade
 from context.context import Context
+
+from utils.benchmark_result_path import generate_benchmark_result_path
+from utils.initial_inventory import initial_inventory_dict
 
 
 class InitialMarketOrderTrader:
@@ -32,6 +36,7 @@ class InitialMarketOrderTrader:
         self.agent_metrics = AgentMetrics()
         self.market_interface = MarketInterface()
         self.first_step_over = False
+        self.forced_execution_done = False
 
     def submit_initial_market_order(self):
         """Submit a market sell order with the entire remaining inventory"""
@@ -51,6 +56,30 @@ class InitialMarketOrderTrader:
         # Set first_step_over flag to True:
         self.first_step_over = True
 
+    def force_execution_episode_end(self):
+        """
+        Submit a market sell order with the entire remaining inventory.
+        Should be triggered, before episode ends.
+        """
+        ticksize = Market.instances["ID"].ticksize
+        best_bid = Market.instances["ID"].best_bid
+        marketable_limit = best_bid - 1000 * ticksize
+
+        remaining_inv = (self.initial_inventory -
+                         self.agent_metrics.executed_quantity)
+
+        # Place order via market interface.
+        if not self.forced_execution_done:
+            self.market_interface.submit_order(side=2,
+                                               limit=marketable_limit,
+                                               quantity=remaining_inv)
+            if self.verbose:
+                print(f'(RL AGENT) Forced Sub: limit: {marketable_limit}  '
+                      f'qt: {remaining_inv}')
+
+            # Set forced execution done flag.
+            self.forced_execution_done = True
+
     def reset(self):
         pass
 
@@ -58,68 +87,101 @@ class InitialMarketOrderTrader:
 # Run backtest.
 if __name__ == '__main__':
 
+    # TODO: I can also iterate over the symbols to let it run in one loop.
+    # SET UP THE BACKTEST:
+    # ---------------------
+    name = "initial_market_Avg-10s-Vol_"
+    symbol = "BAY"
+    # TODO: Use same initial inventory as for RL-agent.
+    initial_inv = initial_inventory_dict[symbol]['Avg-10s-Vol'] * 1_0000
+    testset_start = "2021-05-14" #"2021-01-01" # "2021-05-14"
+    testset_end = "2021-06-30" #"2021-04-30", "2021-06-30"
+    episode_len = "10s"
+    frequency = "1m"
+    num_iterations_to_store_results = 1_000
+    verbose = False
+    # ----------------------
+
     # inst: -> generates episode start list
     # TODO: I HAVE TO USE THE EXACT SAME SAMPLING SET-UP AS IN THE RL-STRATEGY
     #  TO GET THE SAME EPISODE LIST (or better I even save the episode list)
-    replay = Replay(identifier="BAY",
-                    start_date="2021-01-01",
-                    end_date="2021-04-30",
-                    episode_length="10s",
-                    frequency="1m",
+    replay = Replay(identifier=symbol,
+                    start_date=testset_start,  # "2021-05-14",# "2021-01-01",
+                    end_date=testset_end,  # "2021-06-30",#"2021-04-30",
+                    episode_length=episode_len,
+                    frequency=frequency,
                     seed=42,
                     shuffle=False,
                     random_identifier=False,
                     exclude_high_activity_time=False,
+                    verbose=verbose
                     )
+
     # print(replay.episode_start_list)
 
-    # Note: Use base-reset instead of RL reset.
-    # Episode is counted automatically inside episode meaning, everytime
-    # base reset is called, the next episode in episode_start_list is
-    # created.
     results = []
-    # TODO: Problem, I dont know how many of the episodes in episode
-    #  start list can actually be loaded successfulle -> what should I
-    #  iterate over? (zur not try except
-    for episode in range(10):
-        print("episode number:", episode)
+    result_path = generate_benchmark_result_path(symbol=replay.identifier,
+                                                 benchmark_strategy=name)
 
-        # Build new episode and reset env.
-        replay.base_reset()
-        print("episode_start:", replay.episode.episode_start)
+    # Iterate over episodes.
+    for episode in range(1_000_000):  # -> end will not be reached.
 
-        # Initialize agent in each new episode.
-        agent = InitialMarketOrderTrader()
+        # Try as long as there are remaining episodes.
+        try:
 
-        print("Episode Len: ", replay.episode.__len__())
+            # Build new episode and reset env.
+            replay.base_reset()
+            episode_start = replay.episode.episode_start
 
-        # Iterate over Episode Steps.
-        for i in range(replay.episode.__len__()):
+            # Initialize agent in each new episode.
+            agent = InitialMarketOrderTrader(initial_inventory=initial_inv,
+                                             verbose=verbose)
 
-            # Call normal step.
-            replay.normal_step()
+            # Iterate over Episode Steps.
+            for i in range(replay.episode.__len__()):
 
-            # Apply stragey.
-            agent.submit_initial_market_order()
+                # Call normal step.
+                replay.normal_step()
 
-            # The episode ends early when the entire inventory is sold.
-            if (agent.agent_metrics.executed_quantity >=
-                agent.initial_inventory):
-                # Break terminates the loop over the steps and goes to the next
-                # episode.
-                results.append(np.array([replay.episode.episode_start,
-                                         agent.agent_metrics.overall_is(
-                                             scaling_factor=1)]))
-                print(pd.DataFrame(results))
-                break
+                # Apply strategy.
+                agent.submit_initial_market_order()
 
-# Store results to DF:
-# TODO: name vom asset und strategie in filename einbauen.
-path = '/Users/florianewald/Desktop/test_initial_market.csv'
-df = pd.DataFrame(results)
-df.to_csv(path, index=False)
+                # Forced liquidation 2 steps before episode end.
+                if (replay.episode._step == (replay.episode.__len__() - 5) and
+                        not agent.forced_execution_done):
+                    agent.force_execution_episode_end()
+
+                # The episode ends early when the entire inventory is sold.
+                if (agent.agent_metrics.executed_quantity >=
+                        agent.initial_inventory):
+                    # Break terminates the loop over the steps and goes to the next
+                    # episode.
+                    results.append(np.array([replay.episode.episode_start,
+                                             agent.agent_metrics.overall_is(
+                                                 scaling_factor=1),
+                                             agent.agent_metrics.vwap_sell,
+                                             replay.episode._step]))
+
+                    # Intermediate result storage (will be overwritten)
+                    if episode % num_iterations_to_store_results == 0:
+                        df = pd.DataFrame(results,
+                                          columns=["episode_start_time",
+                                                   "is", "vwap_sell",
+                                                   "episode_len"])
+                        df.to_csv(result_path, index=False)
+                        # Print to terminal.
+                        print(df)
 
 
-# TODO: Sicherstellen, dass Market Impact aktiviert ist (wobei es ja eh nur
-#  einen Zeitschritt dauern sollte.
-# TODO: Compute IS and store together with Episode start as Identifier
+                    break
+
+        # Make exception since the episode start list will be over.
+        except:
+            pass
+
+    # Store final  results to DF:
+    df = pd.DataFrame(results, columns=["episode_start_time",
+                                        "is", "vwap_sell", "episode_len"])
+
+    df.to_csv(result_path, index=False)
+    print(df)
