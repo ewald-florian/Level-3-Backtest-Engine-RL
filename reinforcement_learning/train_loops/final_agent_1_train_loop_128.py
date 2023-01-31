389af2cc0dbdf00c78b7d585fccf770bee8673d7
florianewald <mirro.gallier@gmail.com>
2023-01-31 15:57:41 +0100

    fcn128 IS-loop 1 BAY

--- reinforcement_learning/train_loops/final_agent_1_train_loop_128.py
+++ reinforcement_learning/train_loops/final_agent_1_train_loop_128.py
@@ -46,8 +46,8 @@ print("Num GPUs Available TF: ", len(tf.config.list_physical_devices('GPU')))
 
 # Provide checkpoint path if trainer should be restored.
 # TODO: ADD CHECKPOINT PATH
-restoring_checkpoint_path = "/home/jovyan/ray_results/PPO_TradingEnvironment_2023-01-30_16-40-08uht_cgw7/checkpoint_000381"
-name = 'final_agent_1_fcn_128_TWAP_REWARD_BAY_RUN2_'
+restoring_checkpoint_path = "/home/jovyan/ray_results/PPO_TradingEnvironment_2023-01-31_02-53-11byvd0295/checkpoint_000581"
+name = 'final_agent_1_fcn_128_IS_REWARD_BAY_RUN1_'
 
 num_iterations = 200
 save_checkpoints_freq = 10
