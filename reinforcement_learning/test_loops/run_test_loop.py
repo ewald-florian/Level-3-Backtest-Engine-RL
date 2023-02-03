fd5950b06b651de118020b386a3a06e2e5512faa
florianewald <mirro.gallier@gmail.com>
2023-02-03 21:35:35 +0100

    loop

--- reinforcement_learning/test_loops/run_test_loop.py
+++ reinforcement_learning/test_loops/run_test_loop.py
@@ -58,9 +58,9 @@ print("Num GPUs Available TF: ", len(tf.config.list_physical_devices('GPU')))
 #
 # ----------------------------
 # TODO: Insert Agent Name and Symbol!
-STRATEGY_NAME = "A1_FCN_128_LSTM"
+STRATEGY_NAME = "A2_LIMITED"
 AGENT = FinalOEAgent2Limited  # FinalOEAgent2Limited # FinalOEAgent1
-SYMBOL = "BAY"
+SYMBOL = "BMW"
 # ----------------------------
 TEST_START = "2021-05-14"
 TEST_END = "2021-06-30"
@@ -107,7 +107,10 @@ elif STRATEGY_NAME == "A1_BAY":
     CHECKPOINT_PATH = "/home/jovyan/ray_results/PPO_TradingEnvironment_2023-02-01_18-44-573bzdk8pf/checkpoint_000812"
 
 elif STRATEGY_NAME == "A2_LIMITED":
-    CHECKPOINT_PATH = "/Users/florianewald/ray_results/PPOTrainer_TradingEnvironment_2023-02-02_12-40-48qhxzjq6z/checkpoint_000925/checkpoint-925"
+    # Note: I uploaded the weights to the server and run them in a ray 1.x environment "old_ray".
+    CHECKPOINT_PATH = "/home/jovyan/A2_final_weights/A2_final_weights/checkpoint-925"
+    print("*Limited agent path server")
+    #CHECKPOINT_PATH = "/Users/florianewald/ray_results/PPOTrainer_TradingEnvironment_2023-02-02_12-40-48qhxzjq6z/checkpoint_000925/checkpoint-925"
     #CHECKPOINT_PATH = "/Users/florianewald/ray_results/PPOTrainer_TradingEnvironment_2023-01-31_22-31-19agy3bqbz/checkpoint_000401/checkpoint-401"
 
 # Start ray.
